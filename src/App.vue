c725f58fbbf366221572eb7e06f495ad522685c2
nexta <gadressformuyuan@gmail.com>
2024-11-17 00:11:38 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="construction">
+    <h1>🚧 Under 🚧<br>Construction </h1>
+    <p>Wrong time right place!</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'UnderConstruction',
+};
+</script>
+
+<style scoped>
+.construction {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 40ch;
+  text-align: Center;
+}
+
+h1 {
+  font-size: 8vw;
+  margin-bottom: 0rem;
+}
+
+p {
+  font-size: 4vw;
+  font-weight: 300;
+  font-style: italic;
+}
+</style>
