c4e41b5ceb027bc61ceb170ae59d0f01576fc7ab
nexta <gadressformuyuan@gmail.com>
2024-11-30 17:41:45 -0500

    new site

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+  <footer>
+      This website is under construction
+    </footer>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+footer {
+  margin-top: 20px;
+  padding: 10px;
+  color: #666;
+  font-size: 0.9em;
+}
+</style>
